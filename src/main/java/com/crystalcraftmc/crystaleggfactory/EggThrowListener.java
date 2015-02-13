fd04ac74f20fb2c093be96bc91abd491e7665ee0
Alex <www.woodrow73@gmail.com>
2015-02-13 17:02:58 -0500

    Get rid of '+' at start of beginning lines

--- src/main/java/com/crystalcraftmc/crystaleggfactory/EggThrowListener.java
+++ src/main/java/com/crystalcraftmc/crystaleggfactory/EggThrowListener.java
@@ -13,9 +13,9 @@
 + *    See the License for the specific language governing permissions and
 + *    limitations under the License.
 + */
-+
-+
-+package com.crystalcraftmc.crystaleggfactory;
+
+
+package com.crystalcraftmc.crystaleggfactory;
 
 public class EggThrowListener implements Listener {
 	private CakeAnimation accessFields;
